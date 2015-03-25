d9a5e58f82d831aa9f4d9cf56bd1240de1b47cbb
Ryan OConnor <RyanMOConnor@lewisu.edu>
2015-03-25 03:17:14 -0500

    JSON de/serializer using generics

---
+++ WebApplication/WebApplication/Database.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace WebApplication
+{
+    public class Database
+    {
+        private MongoServer Server { get; set; }
+        private MongoDatabase DB { get; set; }
+        private MongoClient dbClient { get; set; }
+        private static Database _instance;
+        public static Database Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Database();
+                return _instance;
+            }
+        }
+
+        public void Start()
+        {
+            dbClient = new MongoClient();
+            Server = dbClient.GetServer();
+            DB = Server.GetDatabase("CloudCrawler");
+        }
+
+        public MongoCollection<T> GetCollection<T>(string name)
+        {
+            MongoCollection<T> collection = null;
+            if(DB.CollectionExists(name))
+            {
+                collection = DB.GetCollection<T>(name);
+            } 
+
+            return collection;
+        }
+    }
+}
---
+++ WebCrawler/WebCrawler/JSON.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace WebCrawler
+{
+    public interface Serializable { }
+    public static class JSON
+    {
+        public static string Serialize<T>(this T obj) where T : Serializable
+        {
+            try
+            {
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    DataContractJsonSerializer ser = new DataContractJsonSerializer(obj.GetType());
+                    ser.WriteObject(stream, obj);
+                    return Encoding.UTF8.GetString(stream.ToArray());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw ex;
+            }
+        }
+
+        public static T Deserialize<T>(string json) where T : Serializable
+        {
+            try
+            {
+                using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+                {
+                    DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(T));
+                    return (T)ser.ReadObject(stream);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw ex;
+            }
+        }
+    }
+}
